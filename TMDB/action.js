ecbab11e98ffa935fb9b1370c02e011d6b220143
Gunadeshwar A <86549002+deshwar005@users.noreply.github.com>
2022-09-27 21:25:04 +0530

    Add files via upload

---
+++ TMDB/action.js
@@ -0,0 +1,38 @@
+const API_KEY='api_key=231d6db3dd9c91cb53282faf7010ecc7';
+const BASE_URL='https://api.themoviedb.org/3';
+const API_url=BASE_URL+'/trending/all/week?'+API_KEY;
+const img_url ='https://image.tmdb.org/t/p/w500';
+const Main=document.getElementById('main');
+GETMOVIE(API_url);
+
+function GETMOVIE(URL)
+{
+ return fetch(URL).then(res => res.json()).then(data =>{
+    console.log(data.results);
+  showmovies(data.results);
+})
+}
+
+function showmovies(data)
+{
+   Main.innerHTML='';
+
+  data.forEach(movie => {
+      const {title,poster_path}=movie;
+      const moviess=document.createElement('div');
+      moviess.classList.add('movie');
+
+      moviess.innerHTML=`
+      <div class="movie ">
+      <img src="${img_url+poster_path}" alt="${title}">
+      <p>${title}</p>
+  </div>`
+  Main.appendChild(moviess);
+
+  });  
+}
+
+function horror()
+{
+  return 
+}
